4e9848b8b8a08f0d945886144b9fb361239551b2
kel2203 <raquelsb2001@gmail.com>
2022-10-13 00:11:17 -0300

    teste commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function pagina(){
+    return <div>
+        <iframe src="../pages/index.html" frameborder="0"scrolling="no" width="1000px" height="900px" ></iframe>
+        dois
+        ds
+    </div>
+}
+
+export default pagina
