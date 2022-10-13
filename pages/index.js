6a33ff8eff570a82d7cb06b24a79575a322d1284
kel2203 <raquelsb2001@gmail.com>
2022-10-13 01:42:44 -0300

    dc

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-function pagina(){
-    return <div>
-        <iframe src="../pages/index.html" frameborder="0"scrolling="no" width="1000px" height="900px" ></iframe>
-        dois
-        ds
-    </div>
-}
-
-export default pagina
